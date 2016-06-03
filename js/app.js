7fcbe3c64dab6865023526eddd9156bd75467a1a
Alexandra Moore <alexmoore94@hotmail.co.uk>
2016-06-03 20:48:33 +0100

    removed angularjs

--- js/app.js
+++ js/app.js
@@ -1,27 +1,16 @@
-var app = angular.module('app', ['ngMessages']);
-
-app.controller('order', ['$scope', function($scope) {
-	$scope.order = {
+var order = {
 		name: null,
 		email: null,
 		message: null
 	};
 
-	$scope.isRequired = function(name) {
-		return $scope.form[name].$touched && $scope.form[name].$pristine  || $scope.form[name].$dirty && $scope.form[name].$invalid;
+
+
+$('#submit').click(function submitOrder() {
+
+	if (order.email == null) {
+		console.warn('yay', order.email);
+	} else {
+		console.warn('boo');
 	};
-
-	$scope.submit = function submitOrder() {
-
-
-		if ($scope.order.email == null) {
-			document.getElementById("ifInvalid").className = "";
-			console.warn('try again', $scope.order);
-
-		} else {
-
-			console.warn('Well done', $scope.order);
-			document.getElementById("ifInvalid").className = "ng-hide";
-		};
-	}
-}]);
+});
