28f462a35264ee0c72af6bc7c338f124b5d0bc5a
Alexandra Moore <alexmoore94@hotmail.co.uk>
2016-06-05 12:31:13 +0100

    added form validation

--- js/app.js
+++ js/app.js
@@ -4,13 +4,75 @@ var order = {
 		message: null
 	};
 
+var valid = {
+	 	name: false,
+	 	email: false,
+	 	message: false
+	};
 
+function isRequired(el) {
+	if ($('#' + el).prop('required')) {
+		return true;
+	} else {
+		return false;
+	};
+};
+
+function isEmpty(el) {
+	if (!$('#' + el).val()) {
+		return true;
+	} else {
+		return false;
+	};
+};
+
+
+function isFormValid(el) {
+	if (isRequired(el)) {
+		if(!isEmpty(el)){
+			valid[el] = true;
+		};
+	};
+};
+
+function errorMsg(el) {
+	$("#" + el + "Err").show();
+	$('.errormsg').show();
+};
+
+function removeErrorMsg(el) {
+	$("#" + el + "Err").hide();
+	$('.errormsg').hide();
+};
 
 $('#submit').click(function submitOrder() {
 
-	if (order.email == null) {
-		console.warn('yay', order.email);
+	order.name = $('#name').val();
+	order.email = $('#email').val();
+	order.message = $('#message').val();
+
+	console.log(order);
+
+	isFormValid('name');
+	isFormValid('email');
+	isFormValid('message');
+
+	if (valid.name == false) {
+		errorMsg('name');
 	} else {
-		console.warn('boo');
+		removeErrorMsg('name');
+	};
+	if (valid.email == false) {
+	errorMsg('email');
+	} else {
+		removeErrorMsg('email');
+	};
+	if (valid.message == false) {
+		errorMsg('message');
+	} else {
+		removeErrorMsg('message');
 	};
 });
+
+
+
